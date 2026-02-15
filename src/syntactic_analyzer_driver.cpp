b9de40faf5d9736460e3e09897a930aa265e78aa
Kapsulon <theo.bary@epitech.eu>
2026-02-14 19:54:55 -0500

    [add] disable output of sets by default

--- src/syntactic_analyzer_driver.cpp
+++ src/syntactic_analyzer_driver.cpp
@@ -2,6 +2,8 @@
 #include "SyntacticAnalyzer/SyntacticAnalyzer.hpp"
 #include "spdlog/spdlog.h"
 
+#define OUTPUT_SETS 0
+
 int main(int argc, char **argv)
 {
     spdlog::set_pattern("[%^%l%$] %v");
@@ -16,12 +18,14 @@ int main(int argc, char **argv)
 
     for (std::uint64_t idx = 1; idx != argc; idx++) {
         syntacticAnalyzer.openFile(argv[idx]);
+#if OUTPUT_SETS
         std::ofstream first(std::string(argv[idx]) + ".out.first");
         std::ofstream follow(std::string(argv[idx]) + ".out.follow");
         first << syntacticAnalyzer.getFirstSet();
         follow << syntacticAnalyzer.getFollowSet();
         first.close();
         follow.close();
+#endif
         syntacticAnalyzer.parse();
     }
 
